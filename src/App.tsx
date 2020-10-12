8e181af1244103e253d17990107c48265a8ca9b0
Jshoes <576504437@qq.com>
2020-10-12 13:38:01 +0800

    feat: cra-ui init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Card, Form, Input, Button, Row, Col } from "antd";
+import "antd/dist/antd.css";
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
 
 function App() {
+  function onFinish(values: Object) {
+    console.log(values);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      <Col span={12} offset={6}>
+        <Card title="创建模块" bordered={false}>
+          <Form onFinish={onFinish} style={{textAlign:'center'}}>
+            <Form.Item
+              label="模版名称"
+              name="modelname"
+              initialValue="modeltest"
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="模版路径"
+              name="modelpath"
+              initialValue="/aaa/bbb/ccc"
+            >
+              <Input />
+            </Form.Item>
+            <Button type="primary" htmlType="submit">
+              提交
+            </Button>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
   );
 }
 
