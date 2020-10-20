92148358289ccd4a6bae637a3bf02589e9e23d86
李响 <>
2020-10-20 17:26:28 +0800

    fix: 增加后端处理修改路由事件

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,31 @@ import ModuleCreator from "./modules/moduleCreator";
 import Packages from './modules/Packages';
 
 function App() {
+  addOwnListener()
   return (
     <Router>
       <Switch>
-        <Route path="/modulecreate" component={ModuleCreator} />Î
-        <Route path="/packages" component={Packages} />
+        <Route exact path="/" component={ModuleCreator} />
+        <Route exact path="/modulecreate" component={ModuleCreator} />
+        <Route exact path="/packages" component={Packages} />
       </Switch>
     </Router>
   );
+}
+
+function addOwnListener() {
+  window.addEventListener('message', event => {
+    const message = event.data; // The JSON data our extension sent
+
+    switch (message.command) {
+        case 'changeRouter':
+            console.log('message:changeRouter:', JSON.stringify(message.text))
+            if (typeof message.text === 'string') {
+              window.location.hash = message.text
+            }
+            break;
+    }
+});
 }
 
 export default App;
--- src/modules/moduleCreator/index.tsx
+++ src/modules/moduleCreator/index.tsx
@@ -9,6 +9,8 @@ function App() {
   const [form] = Form.useForm()
   function onFinish(values: Object) {
     console.log(values);
+    const event = new CustomEvent('sendMessage', { detail: values });
+    document.dispatchEvent(event);
   }
   const tableDataConf = tableDataConfFn(form)
   const listDataConf = listDataConfFn(form)
@@ -32,17 +34,17 @@ function App() {
               <Input />
             </Form.Item>
             <Form.Item label="自定义列表" name="tabelData" initialValue={[]}>
-              <EditTable {...tableDataConf} onChange={(data)=>form.setFieldsValue({
+              <EditTable {...tableDataConf} onChange={(data: any)=>form.setFieldsValue({
                 tabelData:data
               })} data={form.getFieldValue('tabelData')}/>
             </Form.Item>
             <Form.Item label="列表查询" name="listData" initialValue={[]}>
-              <EditTable {...listDataConf} onChange={(data)=>form.setFieldsValue({
+              <EditTable {...listDataConf} onChange={(data: any)=>form.setFieldsValue({
                 listData:data
               })} />
             </Form.Item>
             <Form.Item label="工具按钮" name="buttonData" initialValue={[]}>
-              <EditTable {...buttonDataConf} onChange={(data)=>form.setFieldsValue({
+              <EditTable {...buttonDataConf} onChange={(data: any)=>form.setFieldsValue({
                 buttonData:data
               })} data={form.getFieldValue('buttonData')}/>
             </Form.Item>
